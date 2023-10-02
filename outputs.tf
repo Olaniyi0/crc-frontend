6a8bd25ed208e731004d9e2777dbeb2f06a5f194
olaniyi <niyioladoja0@gmail.com>
2023-10-02 16:50:54 +0100

    changes to terraform apply

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "website-url" {
   value = "${azurerm_dns_cname_record.myresumes.name}.${data.azurerm_dns_zone.myresumes.name}"
 }
+
+output "rg-name" {
+  value = azurerm_resource_group.resume-group.name
+}
+
+output "storage-name" {
+  value = azurerm_storage_account.resume-storage-account.name
+}
+
+output "cdn-profile-name" {
+  value = azurerm_cdn_profile.resume-cdn-profile.name
+}
